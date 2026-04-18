440cdf3d50eb3887b605db5dbec7987278223921
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2026-04-18 20:44:52 -0300

    fix: corrigido uso do 'use client' e removido localhost das chamadas de API

--- src/pages/api/financeiro/importar.ts
+++ src/pages/api/financeiro/importar.ts
@@ -9,6 +9,21 @@ export const config = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("Início do handler importar.ts");
+
+  // Polyfill para DOMMatrix (necessário para versões recentes do pdfjs-dist no Node.js)
+  if (typeof (global as any).DOMMatrix === "undefined") {
+    (global as any).DOMMatrix = class DOMMatrix {
+      a = 1; b = 0; c = 0; d = 1; e = 0; f = 0;
+      m11 = 1; m12 = 0; m13 = 0; m14 = 0;
+      m21 = 0; m22 = 1; m23 = 0; m24 = 0;
+      m31 = 0; m32 = 0; m33 = 1; m34 = 0;
+      m41 = 0; m42 = 0; m43 = 0; m44 = 1;
+      is2D = true;
+      isIdentity = true;
+      constructor() {}
+      toString() { return "[object DOMMatrix]"; }
+    };
+  }
   
   try {
     if (req.method !== "POST") {
